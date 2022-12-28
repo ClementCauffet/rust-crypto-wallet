c06759b8647e6483932fc467e439fc47c5a48d0a
Clement CAUFFET <ccauffet@tech.aubay.com>
2022-12-28 16:24:09 +0100

    seed wallet

--- src/eth_wallet.rs
+++ src/eth_wallet.rs
@@ -3,6 +3,7 @@ use secp256k1::{
     PublicKey, SecretKey,
 };
 
+//key generation (based on the rng variable)
 pub fn generate_keypair() -> (SecretKey, PublicKey) {
     let secp = secp256k1::Secp256k1::new();
     let mut rng = rngs::StdRng::seed_from_u64(1234567890);
