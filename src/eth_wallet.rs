c083ee9ec555193c13ac1d7e0903409c3b6d7cf0
Clement CAUFFET <ccauffet@tech.aubay.com>
2023-01-10 14:44:03 +0100

    Public Key Address

--- src/eth_wallet.rs
+++ src/eth_wallet.rs
@@ -12,3 +12,12 @@ pub fn generate_keypair() -> (SecretKey, PublicKey) {
     let mut rng = rngs::StdRng::seed_from_u64(0123);
     secp.generate_keypair(&mut rng)
 }
+
+pub fn public_key_address(public_key: &PublicKey) -> Address {
+    let public_key = public_key.serialize_uncompressed();
+
+    debug_assert_eq!(public_key[0], 0x04);
+    let hash = keccak256(&public_key[1..]);
+
+    Address::from_slice(&hash[12..])
+}
